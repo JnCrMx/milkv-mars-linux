starfive-tech/linux

soc: sifive: ccache: Add sifive_l2_flush64_range
Add sifive_l2_flush64_range to be compatible with old code

Signed-off-by: Samin Guo <samin.guo@starfivetech.com>

drivers/soc/sifive/sifive_ccache.c
@@ -156,6 +156,12 @@ void sifive_ccache_flush_range(phys_addr_t start, size_t len)
 }
 EXPORT_SYMBOL_GPL(sifive_ccache_flush_range);
 
+void sifive_l2_flush64_range(phys_addr_t start, size_t len)
+{
+	sifive_ccache_flush_range(start, len);
+}
+EXPORT_SYMBOL_GPL(sifive_l2_flush64_range);
+
 void *sifive_ccache_set_uncached(void *addr, size_t size)
 {
 	phys_addr_t phys_addr = __pa(addr) + uncached_offset;

JnCrMx/milkv-mars-linux

Signed-off-by: Hal Feng <hal.feng@starfivetech.com>

drivers/cache/sifive_ccache.c
@@ -162,6 +162,12 @@ static void ccache_flush_range(phys_addr_t start, size_t len)
 	}
 }
 
+void sifive_l2_flush64_range(phys_addr_t start, size_t len)
+{
+	ccache_flush_range(start, len);
+}
+EXPORT_SYMBOL_GPL(sifive_l2_flush64_range);
+
 static const struct riscv_nonstd_cache_ops ccache_mgmt_ops __initconst = {
 	.wback = &ccache_flush_range,
 	.inv = &ccache_flush_range,